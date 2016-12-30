65d8c6ade861153c6e730ca851762d3a40bb41c4
Kasia <Kasia@charlie>
2016-12-30 20:13:29 +0100

    added ManuPane controller

---
+++ Youtufy/src/awolny/youtufy/controller/MenuPaneController.java
@@ -0,0 +1,23 @@
+package awolny.youtufy.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+
+public class MenuPaneController implements Initializable {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+
+	}
+}
