871615c85ebc824fade09fb34caed444b9396f61
Kasia <Kasia@charlie>
2016-12-30 21:07:51 +0100

    created controller

---
+++ Youtufy/src/awolny/youtufy/controller/MainController.java
@@ -0,0 +1,20 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+
+public class MainController implements Initializable {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+	}
+}
